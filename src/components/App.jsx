728f849f511eeb268a0f3da0404892e352cadc8b
saivenkat1910 <saiuddagir1235@gmail.com>
2022-06-08 11:05:22 +0530

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import Header from "./Header";
+import  Note from "./Note";
+import CreateArea from "./CreateArea";
+
+export default function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+    </div>
+  );
+}
+//function createNotes(propss){
+//   return <Note
+//   key={propss.key}
+//   title={propss.title}
+//   content={propss.content} />
+// }
---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,32 @@
+import React,{useState} from "react";
+export default function CreateArea(props) {
+  const [note, setNote] = useState({
+    title:"",
+    content:""
+  });
+  function handleChange(event){
+   const {name,value}=event.target;
+   setNote((prevNote=>{
+     return {...prevNote,[name]:value}
+   }))
+  }
+
+  function submitNote(event)
+  {
+    props.onAdd(note);
+    setNote({
+      title:"",
+      content:""
+    })
+    event.preventDefault();
+  }
+  return (
+    <div>
+      <form>
+        <input name="title" value={note.title}  onChange={handleChange} placeholder="Title" />
+        <textarea name="content" value={note.content}  onChange={handleChange} placeholder="write notes" />
+        <button onClick={submitNote}>add</button>
+      </form>
+    </div>
+  );
+}
